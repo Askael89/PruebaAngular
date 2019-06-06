4bd065e16e77c8b23c760a13f2e985867c64dda2
Askael89 <rogeralvarezakel@hotmail.com>
2019-06-06 12:12:15 -0500

    Segundo Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PadreComponent } from './Componentes/padre/padre.component';
+import {HijoComponent} from './Componentes/hijo/hijo.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'padre', pathMatch: 'full'},
+  {path: 'padre', component: PadreComponent, children: [
+    {path: 'hijo', component: HijoComponent}
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PadreComponent } from './Componentes/padre/padre.component';
+import { HijoComponent } from './Componentes/hijo/hijo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule,
